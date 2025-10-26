e3a1bdca6ae2ef37b3d777ab0589140d05a71ea7
yanis-fellache <yanis.fellache@gmail.com>
2025-10-25 22:09:01 -0400

    Created hint

---
+++ src/components/hint.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import trophyImage from '../assets/cr_trophy.png';
+import "../css/hint.css";
+
+function Hint() {
+
+    return (
+        <>
+            <div class="container">
+                <div class="box">Centered Box</div>
+            </div>
+        </>
+    );
+}
+
+export default Hint;
