f8603f2bc9661ec59b6f73c4a8915d8253431338
Alejo435 <aoterminp@icloud.com>
2025-10-25 20:30:53 -0400

    change to submit button

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,4 @@
 import React from "react"
-//call api
-//return id question incorrect cards correct cards
-//Change codingProblem text to question
-//for cards we want an array with incorrect and correct cards randomized then add the text to seperate cards
 function SubmitButton() {
 
     function onSubmit() {
